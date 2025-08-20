77eeb889a99afc04426a406fea8fc9deac4cdd3d
Magdalena Szymura <mag.szymura@gmail.com>
2025-08-20 19:09:31 +0200

    SAMOUK-2 feat: add select component

--- src/app/start-screen/start-screen.ts
+++ src/app/start-screen/start-screen.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { InputText } from '../input-text/input-text';
+import { Select } from '../select/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'start-screen',
-  imports: [InputText],
+  imports: [InputText, Select, FormsModule],
   templateUrl: './start-screen.html',
   styleUrl: './start-screen.scss',
 })
-export class StartScreen {}
+export class StartScreen {
+  selectedDifficulty = 'easy';
+}
