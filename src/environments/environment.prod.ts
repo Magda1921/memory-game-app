7f0c28203379c4f90dde56ba6a5a5d207d451286
Magdalena Szymura <mag.szymura@gmail.com>
2025-09-04 19:01:31 +0200

    SAMOUK-3 fix: remove environments

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  UNSPLASH_API_KEY: 'U0Idyxs9bDIH4_sgybQiaXfMDlw16_VSI8BLM9sSueU',
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  UNSPLASH_API_KEY: 'U0Idyxs9bDIH4_sgybQiaXfMDlw16_VSI8BLM9sSueU'
-};
