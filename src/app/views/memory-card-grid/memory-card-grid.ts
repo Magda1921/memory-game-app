46f9c7ea57d9d30d0820c08554e7cda294e05b4d
Magdalena Szymura <mag.szymura@gmail.com>
2025-09-24 16:49:31 +0200

    SAMOUK-5 feat: add card reveal and pair checking

--- src/app/views/memory-card-grid/memory-card-grid.ts
+++ src/app/views/memory-card-grid/memory-card-grid.ts
@@ -11,6 +11,7 @@ import { UnsplashPhoto } from '@app/types/unsplash';
 export class MemoryCardGrid implements OnInit {
   images = signal<string[]>([]);
   countOfImages = input.required<number>();
+  flippedCards = signal<Set<HTMLElement>>(new Set());
 
   constructor(private unsplashService: Unsplash) {}
 
@@ -18,7 +19,53 @@ export class MemoryCardGrid implements OnInit {
     this.unsplashService
       .fetchImages('nature', this.countOfImages())
       .subscribe((photos: UnsplashPhoto[]) => {
-        this.images.set(photos.map((photo) => photo.urls.small));
+        const doubledImages = photos
+          .map((photo) => photo.urls.small)
+          .flatMap((url) => [url, url]);
+        const shuffledPhotos = this.shuffle(doubledImages);
+        this.images.set(shuffledPhotos);
       });
+  }
+
+  flipCard(event: Event): void {
+    if (this.flippedCards().size < 2) {
+      const card = event.currentTarget as HTMLElement;
+      this.flippedCards.set(this.flippedCards().add(card));
+      card.classList.toggle('card--flipped');
+      if (this.flippedCards().size === 2) {
+        const [firstCard, secondCard] = Array.from(this.flippedCards());
+        this.checkPair(firstCard, secondCard);
+      }
+    } else {
+      return;
+    }
+  }
+  private shuffle(array: string[]): string[] {
+    let currentIndex: number = array.length;
+
+    while (currentIndex !== 0) {
+      const randomIndex: number = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+  private checkPair(card1: HTMLElement, card2: HTMLElement): void {
+    const img1 = card1.querySelector('img')?.src;
+    const img2 = card2.querySelector('img')?.src;
+    if (img1 === img2) {
+      this.flippedCards.set(new Set());
+      return;
+    } else {
+      setTimeout(() => {
+        card1.classList.remove('card--flipped');
+        card2.classList.remove('card--flipped');
+        this.flippedCards.set(new Set());
+      }, 3000);
+    }
   }
 }
